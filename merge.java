6e94acce7d9d69ad6a3f0cf2f745fcef8079a245
BrandonR451 <reaganb45@yahoo.com>
2026-04-28 23:48:42 -0500

    Update merge.java

--- merge.java
+++ merge.java
@@ -8,6 +8,39 @@ package pharmacy_inventory_management_system;
  *
  * @author Brandon Reagan
  */
-public class Medication {
-    
+public class merge (List<Medication> list, List<Medication> left, List<Medication> right) {
+    // i = index for left list
+        // j = index for right list
+        // k = index for main list
+        int i = 0, j = 0, k = 0;
+        
+        // Compare elements from left and right lists
+        while (i < left.size() && j < right.size()) {
+            
+             // Compare expiration dates
+            // If left date is earlier, place it first
+            if (left.get(i).getExpirationDate().before(right.get(j).getExpirationDate())) {
+                
+                // Add left element into main list
+                list.set(k++, left.get(i++));
+                
+            } 
+            
+            else {
+                
+                 // Otherwise, add right element
+                list.set(k++, right.get(j++));
+            }
+        }
+         // Add remaining elements from left list (if any)
+        while (i < left.size()) {
+            list.set(k++, left.get(i++));
+        }
+        
+        // Add remaining elements from right list (if any)
+        while (j < right.size()) {
+            list.set(k++, right.get(j++));
+        }
+
+    }
 }
