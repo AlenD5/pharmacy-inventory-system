ca698e06b76005c421cc4b9187f5c2a778df99d9
BrandonR451 <reaganb45@yahoo.com>
2026-04-29 00:21:15 -0500

    Update loadLowStockQueue.java

--- loadLowStockQueue.java
+++ loadLowStockQueue.java
@@ -5,16 +5,19 @@
 package pharmacy_inventory_management_system;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import static pharmacy_inventory_management_system.Pharmacy_Inventory_Management_System.inventoryMap;
+import static pharmacy_inventory_management_system.Pharmacy_Inventory_Management_System.lowStockQueue;
 
 /**
  *
  * @author Brandon Reagan
  */
-public class loadLowStockQueue(Connection conn) throws Exception {
-
-      // ================= SQL QUERY =================
+public class loadLowStockQueue {
+      // LOAD ONLY LOW STOCK → QUEUE
+    public static void loadLowStockQueue(Connection conn) throws Exception {
+        
+            // ================= SQL QUERY =================
             
         // SQL query to get ONLY medications with low stock (quantity < 20)
         // Uses JOIN to combine medication details with inventory data
@@ -51,3 +54,4 @@ public class loadLowStockQueue(Connection conn) throws Exception {
             lowStockQueue.add(m);
         }
     }
+}
